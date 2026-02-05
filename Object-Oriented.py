44eea97ff47650eb091bb7725e0afebde3494919
TaylorCArmstrong <jtarmstrong2019@gmail.com>
2026-02-05 11:44:10 -0700

    third commit

--- Object-Oriented.py
+++ Object-Oriented.py
@@ -3,7 +3,7 @@ import re
 #Part 1: Class Definition
 
 #class of student
-class Student:
+class student:
     def __init__(self, name='', email=''):
         self.name = name
         self.email = email
@@ -29,13 +29,13 @@ class Student:
 
 #Create 3 student objects with initial grades
 
-Student1 = Student ("Annie", "annie@gmail.com")
+Student1 = student ("Annie", "annie@gmail.com")
 Student1.grades[98, 75, 80]
 
-Student2 = Student ("Karl", "karl@Yahoo.com")
+Student2 = student ("Karl", "karl@Yahoo.com")
 Student2.grades[81, 79, 92]
 
-Student3 = Student ("Walker", "walker@outlook.com")
+Student3 = student ("Walker", "walker@outlook.com")
 Student3.grades[98, 94, 100]
 
 #Adding 2 new grades for each student
@@ -51,9 +51,9 @@ Student3.add_grade(92)
 
 #Printing the average of each student
 
-Students = [Student1, Student2, Student3]
+students = [Student1, Student2, Student3]
 
-for s in Students:
+for s in students:
     s.display_info()
     print(f"Average Grade: {s.average_grade()}")
     print("-" * 30)
@@ -97,3 +97,29 @@ try:
     grades_as_tuple[0] = 999
 except TypeError:
     print("Tuples are immutable, their values cannot be changed.")
+
+#Part 5: List Operations
+
+#Removing the last grade from each student using .pop()
+
+for student in students:
+    removed = student.grades.pop()
+    print(f"Removed last grade({removed}) from {student.name}")
+
+#Accessing the first and last grade for each student
+
+for Student in students:
+    if student.grades:
+        first = student.grades[0]
+        last = student.grades[-1]
+        print(f"{student.name}'s first grade: {first}")
+        print(f"{student.name}'s last grade: {last}")
+    else:
+        print(f"{student.name} has no grades left.")
+
+#Print the number of grades for each student
+
+for student in students:
+    print(f"{student.name} has {len(student.grades)} grades.")
+
+    
